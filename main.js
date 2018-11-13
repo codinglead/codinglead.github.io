f2fdaaa2d10f4495d145336a34c4024ec787d58a
codinglead <realcodinglead@gmail.com>
2018-11-13 04:21:24 -0800

    add simple esearch for the blogs

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ menuHamburger.addEventListener('click', function () {
   menuHamburger.classList.toggle('animate');
   responsiveNav.classList.toggle('responsive-nav__open')
 });
+
+let searchBox = document.getElementById('search-input');
+let searchBoxResultContainer = document.getElementsByClassName('search')[0];
+
+searchBox.addEventListener('keyup', function () {
+
+  if (!(searchBoxResultContainer.classList[1] === 'hide') && searchBox.value === '') {
+    console.log('called addded')
+    searchBoxResultContainer.classList = 'search hide';
+  } else if (searchBox.value !== '') {
+    searchBoxResultContainer.classList = 'search';
+  }
+})
