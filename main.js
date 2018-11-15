47cd52a1dfc44455f54fd1d63ca7d70491cdfa02
codinglead <realcodinglead@gmail.com>
2018-11-14 17:07:51 -0800

    fix a bug when searching for post, it will not take up the whole screen

--- main.js
+++ main.js
@@ -8,13 +8,19 @@ menuHamburger.addEventListener('click', function () {
 
 let searchBox = document.getElementById('search-input');
 let searchBoxResultContainer = document.getElementsByClassName('search')[0];
+let mainContent = document.querySelector('.container .posts')
 
 searchBox.addEventListener('keyup', function () {
-
+  // if on the search box and the value is empty string
+  // hide the search results
+  // show all the post on the page
+  // otherwise show the results
+  // hide the post on the page
   if (!(searchBoxResultContainer.classList[1] === 'hide') && searchBox.value === '') {
-    console.log('called addded')
     searchBoxResultContainer.classList = 'search hide';
+    mainContent.classList = 'posts';
   } else if (searchBox.value !== '') {
     searchBoxResultContainer.classList = 'search';
+    mainContent.classList = 'posts hide';
   }
 })
