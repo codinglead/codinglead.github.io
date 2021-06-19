51de2a50872689fcabac648bb93ab83d97a8f5f1
codinglead <realcodinglead@gmail.com>
2021-06-18 19:56:27 -0700

    add condition to check if search box exist before using it

--- main.js
+++ main.js
@@ -7,20 +7,21 @@ menuHamburger.addEventListener('click', function () {
 });
 
 let searchBox = document.getElementById('search-input');
-let searchBoxResultContainer = document.getElementsByClassName('search')[0];
-let mainContent = document.querySelector('.container .posts')
-
-searchBox.addEventListener('keyup', function () {
-  // if on the search box and the value is empty string
-  // hide the search results
-  // show all the post on the page
-  // otherwise show the results
-  // hide the post on the page
-  if (!(searchBoxResultContainer.classList[1] === 'hide') && searchBox.value === '') {
-    searchBoxResultContainer.classList.add('hide');
-    mainContent.classList = 'posts';
-  } else if (searchBox.value !== '') {
-    searchBoxResultContainer.classList.remove('hide');
-    mainContent.classList = 'posts hide';
-  }
-})
+if (searchBox) {
+    let searchBoxResultContainer = document.getElementsByClassName('search')[0];
+    let mainContent = document.querySelector('.container .posts')
+    searchBox.addEventListener('keyup', function () {
+        // if on the search box and the value is empty string
+        // hide the search results
+        // show all the post on the page
+        // otherwise show the results
+        // hide the post on the page
+        if (!(searchBoxResultContainer.classList[1] === 'hide') && searchBox.value === '') {
+            searchBoxResultContainer.classList.add('hide');
+            mainContent.classList = 'posts';
+        } else if (searchBox.value !== '') {
+            searchBoxResultContainer.classList.remove('hide');
+            mainContent.classList = 'posts hide';
+        }
+    });
+}
